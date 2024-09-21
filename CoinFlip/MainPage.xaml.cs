51077a91fe0c4398e5fea842d44dfcda97c7535c
Etec Prof. Horácio Augusto da Silveira <enzo_negrao_freitas@outlook.com>
2024-09-20 22:54:12 -0300

    Projeto Criado

---
+++ CoinFlip/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace CoinFlip
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void FlipButton_Clicked(object sender, EventArgs e)
+        {
+            int escolha = SelectedSide.SelectedIndex;
+            Moeda moeda = new Moeda(escolha);
+            ResultLabel.Text = moeda.Jogar();
+        }
+        // 0 = cara; 1 = coroa;
+    }
+
+}
---
+++ CoinFlip/Moeda.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinFlip
+{
+    internal class Moeda
+    {
+        private int lado;
+
+        public Moeda(int Lado)
+        {
+            lado = Lado;
+        }
+
+        public string Jogar()
+        {
+            int Result = new Random().Next(0, 2);
+            if (Result == lado)
+            {
+                return "Você venceu";
+            }
+            else
+            {
+                return "Você perdeu";
+            }
+        }
+    }
+}
